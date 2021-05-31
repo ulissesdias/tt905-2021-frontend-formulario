8ad3cbd4c296abc994a37e0f95b032c78a88d997
Ulisses Dias <ulisses@ft.unicamp.br>
2021-05-31 11:03:22 -0300

    adicionando mudanças

--- js/functions.js
+++ js/functions.js
@@ -16,5 +16,37 @@ async function callFetchWithGet(){
     } else {
         console.log("Deu errado");
     }
+}
 
+async function callFetchWithPost(mensagem){
+    const options = {
+        method : 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept' : 'application/json',
+            'content-type' : 'application/json'
+        },
+        body :JSON.stringify({
+            'mensagem' : mensagem
+        })
+    }
+    await fetch(url, options);
+}
+
+async function callFetchWithPut(){
+    let headers = new Headers();
+    const options = {
+        method : 'GET',
+        mode: 'cors',
+        headers: headers
+    }
+}
+
+async function callFetchWithDelete(){
+    let headers = new Headers();
+    const options = {
+        method : 'GET',
+        mode: 'cors',
+        headers: headers
+    }
 }
