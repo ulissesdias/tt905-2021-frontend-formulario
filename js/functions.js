cbb505096f307c932deef89a1e6edb3a33ab4279
Ulisses Dias <ulisses@ft.unicamp.br>
2021-05-31 11:35:41 -0300

    Código completo

--- js/functions.js
+++ js/functions.js
@@ -59,3 +59,31 @@ async function callFetchWithDelete(id){
     }
     await fetch(`${url}${id}`, options);
 }
+
+/*
+    Formulários
+*/
+
+function submitPost(){
+    console.log("entrei na função");
+    
+    const form = document.forms['postForm'];    
+    const mensagem = form["mensagem"].value;
+    callFetchWithPost(mensagem);
+    return false; // Evitar o reload da tela.
+}
+
+function submitPut(){
+    const form = document.forms['putForm'];  
+    const id = form["id"].value;  
+    const mensagem = form["mensagem"].value;
+    callFetchWithPut(id, mensagem);
+    return false; // Evitar o reload da tela.
+}
+
+function submitDelete(){
+    const form = document.forms['deleteForm'];  
+    const id = form["id"].value;  
+    callFetchWithDelete(id);
+    return false; // Evitar o reload da tela.
+}
